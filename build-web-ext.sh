45051afac3629177c9cb28779bbab6bafba05b4b
Andrew Shu <talklittle@gmail.com>
2025-09-05 08:40:24 -0700

    Move web-ext config to web-ext-config.mjs

--- build-web-ext.sh
+++ build-web-ext.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-web-ext build \
-  --ignore-files \
-  $0 \
-  README.md \
-  screenshot1.jpeg \
-  screenshot2.jpeg \
-  get-the-addon-178x60px.dad84b42.png
+web-ext --config=web-ext-config.mjs build
---
+++ web-ext-config.mjs
@@ -0,0 +1,10 @@
+export default {
+  ignoreFiles: [
+    'build-web-ext.sh',
+    'web-ext-config.mjs',
+    'README.md',
+    'screenshot1.jpeg',
+    'screenshot2.jpeg',
+    'get-the-addon-178x60px.dad84b42.png',
+  ],
+};
